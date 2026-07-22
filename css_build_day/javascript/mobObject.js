fbfac072e2e1ca9e5d30b37f5d40ffb07820fc29
Puttipong Runtakit <puttipong.runtakit@gmail.com>
2026-07-22 14:57:19 +0700

    first_commit css_build_day

---
+++ css_build_day/javascript/mobObject.js
@@ -0,0 +1,10 @@
+const myObject = [
+  {
+    houseImage: "assets/home1/jpg",
+    name: "บ้านในกรุงเทพ",
+    price: 3000,
+    descript: "สำหรับ 2 คืน 5",
+  },
+];
+
+myObject.forEach((product) => {});
